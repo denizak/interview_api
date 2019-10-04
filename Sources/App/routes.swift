9dc30b5381fbd41e773d6c85e735f9bafead5a97
Deni <denizakya@gmail.com>
2019-10-04 09:18:06 +0700

    Remove unused Todo

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,12 +7,6 @@ public func routes(_ router: Router) throws {
         return "It works!"
     }
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
-
     filmRoute(router)
 }
 
