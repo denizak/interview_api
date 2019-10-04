2f7aa4e04f07aa10c1527620a266a406ed4565e5
Deni <denizakya@gmail.com>
2019-10-04 10:58:44 +0700

    Add playableUrl and imageUrl in Film

--- Sources/App/Models/Film.swift
+++ Sources/App/Models/Film.swift
@@ -4,10 +4,17 @@ import Vapor
 final class Film: SQLiteModel {
     var id: Int?
     var title: String
+    var playableUrl: String
+    var imageUrl: String
 
-    init(id: Int? = nil, title: String) {
+    init(id: Int? = nil,
+         title: String,
+         playableUrl: String,
+         imageUrl: String) {
         self.id = id
         self.title = title
+        self.playableUrl = playableUrl
+        self.imageUrl = imageUrl
     }
 }
 
