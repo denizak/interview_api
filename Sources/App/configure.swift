642c0f5faf43b0c4d9381cf44f59a6ea34367204
Deni <denizakya@gmail.com>
2019-10-06 13:27:12 +0700

    Move configuration into its function

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,29 +3,39 @@ import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    // Register providers first
-    try services.register(FluentSQLiteProvider())
+    try registerProvider(&services)
+    try registerRouter(&services)
+    registerMiddleware(&services)
+    try registerDatabase(&services)
+    configureMigration(&services)
+}
 
-    // Register routes to the router
+private func registerProvider(_ services: inout Services) throws {
+    try services.register(FluentSQLiteProvider())
+}
+
+private func registerRouter(_ services: inout Services) throws {
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
+}
 
+private func registerMiddleware(_ services: inout Services) {
     // Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
+}
 
-    // Configure a SQLite database
+private func registerDatabase(_ services: inout Services) throws {
     let sqlite = try SQLiteDatabase(storage: .memory)
-
-    // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     databases.add(database: sqlite, as: .sqlite)
     services.register(databases)
+}
 
-    // Configure migrations
+private func configureMigration(_ services: inout Services) {
     var migrations = MigrationConfig()
     migrations.add(model: Film.self, database: .sqlite)
     migrations.add(model: Genre.self, database: .sqlite)
